9e3429f9517b818e9c776552950b111ab4780366
Le-Fox <smkupa@gmail.com>
2023-05-03 02:30:39 +0200

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const {data} = api.posts.getAll.useQuery();
   const user = useUser();
 
   return (
@@ -19,7 +19,9 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton/>}
           {!!user.isSignedIn && <SignOutButton/>}
         </div>
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) =>(<div key = {post.id}>{post.content}</div>) )}
+        </div>
       </main>
     </>
   );
