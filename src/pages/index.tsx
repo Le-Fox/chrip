f33e39f5ec59969a4a627876ff0e283bc1d6c2fb
Le-Fox <smkupa@gmail.com>
2023-05-04 03:29:23 +0200

    workig posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,29 @@ import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime)
 
 
 const CreatePostWizard = () => {
   const {user}= useUser();
+  const [input,setInput] =useState<string>("")
+  const ctx = api.useContext()
+
+  const {mutate,isLoading: isPosting} = api.posts.create.useMutation({
+    onSuccess:() => {
+      setInput("");
+       void ctx.posts.getAll.invalidate()
+
+    }
+  });
  
   if (!user) return null;
-  console.log(user.id)
   return <div className="flex gap-3 w-full">
     <Image src={user.profileImageUrl} alt={"Profile image"} className="w-14 h-14 rounded-full" width={56} height={56}/>
-    <input placeholder="Type some emojis!" className="bg-transparent grow outline-none"/>
+    <input placeholder="Type some emojis!" className="bg-transparent grow outline-none" type="text" value={input} onChange={(e) => setInput(e.target.value)}  disabled={isPosting} />
+    <button onClick={() => mutate({content:input})}> Post </button>
   </div>
 }
 
@@ -36,9 +47,7 @@ const PostView = (props:PostWithUser) => {
           <span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
         <span className="text-2xl"> {post.content}</span>
-      </div>
-      
-      
+      </div>    
     </div>
   )
 
@@ -53,7 +62,7 @@ const Feed = () =>{
 
   return (
     <div className="flex flex-col">
-            {[...data,...data]?.map((fullPost) => (
+            {data.map((fullPost) => (
               <PostView {...fullPost} key={fullPost.post.id}/>
            ))}
           </div>
